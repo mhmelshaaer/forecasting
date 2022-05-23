b36c1d65a719795cc80b87eada0c3849f4b59afe
mhmelshaaer <m.elshaaer@dxpr.com>
2022-05-24 01:43:03 +0200

    Using the server_interface to implement a server:
    - Creating my custom messages that I want the system to pass around and use it to implement the server_interface.
    - Starting the server in a while loop that is listening for connections.
    - Actually this server will show nothing since we did not implement the OnClientConnect(), OnClientDisconnect(), and OnMessage().

--- server/main.cpp
+++ server/main.cpp
@@ -1,6 +1,37 @@
 #include <iostream>
 
+#include <libnet.hpp>
+
+enum class CustomMsgTypes : uint32_t
+{
+	ServerAccept,
+	ServerDeny,
+	ServerPing,
+	ServerMessage,
+};
+
+class CustomServer : public olc::net::server_interface<CustomMsgTypes>
+{
+public:
+	CustomServer(uint16_t nPort) : olc::net::server_interface<CustomMsgTypes>(nPort)
+	{
+
+	}
+protected:
+	virtual bool OnClientConnect(std::shared_ptr<olc::net::connection<CustomMsgTypes>> client){}
+    virtual void OnClientDisconnect(std::shared_ptr<olc::net::connection<CustomMsgTypes>> client){}
+    virtual void OnMessage(std::shared_ptr<olc::net::connection<CustomMsgTypes>> client, olc::net::message<CustomMsgTypes>& msg){}
+};
+
+bool bQuit = false;
+
 int main() {
+	CustomServer server(60000);
+	server.Start();
+
+    while(!bQuit) {
+        server.Update();
+    }
 
 	return 0;
 }
